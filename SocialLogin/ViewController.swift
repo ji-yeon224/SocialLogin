a764963bf7b2b2c0f80b3df0bb7efa4f8e9297b2
ji-yeon224 <69784492+ji-yeon224@users.noreply.github.com>
2023-12-28 10:11:34 +0900

    Initial Commit

---
+++ SocialLogin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SocialLogin
+//
+//  Created by 김지연 on 12/28/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
